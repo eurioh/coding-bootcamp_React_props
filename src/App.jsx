9d443f8aab304faaab4b4a3987b59b28f9833685
eurioh <euri.elliot.oh@gmail.com>
2020-02-01 17:20:20 -0500

    add an array of data in contacts.js simplifying props

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,42 @@
 import React from "react";
 import Card from "./components/Card";
+import contacts from "./contacts.js";
 
 function App() {
   return (
     <div>
       <h1>My Contacts</h1>
+    
+      <Card 
+        name = {contacts[0].name}
+        img = {contacts[0].img}
+        alt={contacts[0].alt}
+        tell={contacts[0].tell}
+        email={contacts[0].email}
+     />
     <Card 
-      name="Elizabeth Keen" 
-      img="https://lh3.googleusercontent.com/proxy/UIg-QCCsXdy0-Oa2UIoDErhHFVg7dokSTtT49gfjoQlCqL_ZraO0hGFDWebKwwW-fr-qB_IhYdjBq68QtoX9Sv_hZiUySd2wZGzNIhss_J-S8-UfGfatqYfkS8kcReJrQ_jYum7l2mecD55WbKbfMiNS0Nv-w5YAMa-miN8PuGE" 
-      alt="Lizzy" 
-      tell="+123 456 789" 
-      email="elizabeth.keen@fbi.gov"/>
+        name = {contacts[1].name}
+        img = {contacts[1].img}
+        alt={contacts[1].alt}
+        tell={contacts[1].tell}
+        email={contacts[1].email}
+     />
 
     <Card 
-      name="Raymond Reddington" 
-      img="https://cdnb.artstation.com/p/assets/images/images/008/997/001/large/ahmed-ouardani-a-o-reddington-photo.jpg?1516558606" 
-      alt="Red" 
-      tell="+987 654 321" 
-      email="raymond.red@navy.mil"/>
-    <Card 
-      name="Dembe Zuma" 
-      img="https://vignette.wikia.nocookie.net/the-blacklist/images/d/d3/Dembe_Zuma-Staffel_2.png/revision/latest/top-crop/width/360/height/450?cb=20150409110512&path-prefix=de" 
-      alt="Dembe" 
-      tell="+918 372 574" 
-      email="dembezuma@red.com"/>
+            name = {contacts[2].name}
+            img = {contacts[2].img}
+            alt={contacts[2].alt}
+            tell={contacts[2].tell}
+            email={contacts[2].email}
+        />
 
     <Card 
-      name="Christopher Hargrave" 
-      img="https://i.pinimg.com/originals/1e/a7/e3/1ea7e3e371cb9fc503bd75511c0225e3.jpg" 
-      alt="Tom" 
-      tell="+918 372 574" 
-      email="tom.keen@jes.edu"/>
+                name = {contacts[3].name}
+                img = {contacts[3].img}
+                alt={contacts[3].alt}
+                tell={contacts[3].tell}
+                email={contacts[3].email}
+            />
 
     </div>
   );
